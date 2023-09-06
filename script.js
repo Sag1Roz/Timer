a45eb890bf6a1d03b5a160aeafe0d0b992a40bf9
Sag1Roz <sagi67070@gmail.com>
2023-09-06 19:44:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const display = document.querySelector('#display')
+const div = document.createElement('div')
+let sec = 0
+let minutes = 0
+let hour = 0
+let startTime
+
+
+renderToHtml()
+
+
+function start(){
+    startTime =  setInterval(up,1000);
+
+}
+
+
+
+function up(){
+    if(sec === 59){
+    if(minutes === 59){
+        hour++
+        minutes=0
+    }
+    sec=0
+    minutes++
+}
+sec++
+ renderToHtml()
+}
+
+
+function restart(){
+    sec=0
+    minutes=0
+    hour=0
+    stop()
+   renderToHtml()
+}
+
+function stop(){
+   clearInterval(startTime)
+    renderToHtml()
+}
+
+
+function renderToHtml(){
+    div.innerHTML = `<div class="mid">
+<div class="container">
+ <div class="time">${hour < 10 ? "0" + hour : hour}:${minutes < 10 ? "0" + minutes : minutes}:${sec < 10 ? "0" + sec : sec}</div>
+ <div class="buttes">
+ <button onclick="stop()">Stop</button>
+ <button onclick="start()">Start</button>
+ <button  onclick="restart()">Restart</button>
+ </div>
+</div>
+</div>`
+
+display.appendChild(div)
+}
